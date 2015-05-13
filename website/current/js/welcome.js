f25dd38f91f757d1b3a0915ad62fc250db778100
k11mamaes <Mason@MasonM.local>
2015-05-12 17:57:22 -0700

    Website

--- website/current/js/welcome.js
+++ website/current/js/welcome.js
@@ -4,3 +4,17 @@ $(function() {	//handles logout
             window.location = "index.html";
         });
 });
+
+$(document).ready(function() {
+    checkID()
+});
+
+function checkID(){
+    var adminID = window.localStorage.getItem("adminID");
+    if (adminID != null){	//cookie exists
+        if (adminID < 1) {	//invalid adminID
+            window.localStorage.clear();
+        }
+    }
+    else window.location="index.html";	//cookie doesn't exist, redirect to login page
+}
